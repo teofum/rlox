2585ea0ea1b4705d1aa13a84cf9320d53d3c0d13
Teo Fumagalli <teo.fum@gmail.com>
2024-10-21 22:37:38 -0300

    parser cleanup and comments

--- src/rlox/parser.rs
+++ src/rlox/parser.rs
@@ -3,6 +3,8 @@ use crate::rlox::error::{ErrorType, Logger, LoxError};
 use crate::rlox::token::{Token, TokenType};
 use std::iter::Peekable;
 
+type Result<T> = std::result::Result<T, LoxError>;
+
 pub struct Parser<'a> {
     token_buf: Vec<Token>,
     tokens: Peekable<&'a mut dyn Iterator<Item=Token>>,
@@ -15,89 +17,98 @@ impl<'a> Parser<'a> {
     }
 
     pub fn parse(&mut self) -> Option<Expr> {
-        self.expression()
+        match self.expression() {
+            Ok(expr) => Some(expr),
+            Err(err) => {
+                self.logger.log(err);
+                None
+            }
+        }
     }
 
+    /// Helper function to parse productions with a binary, left-associative operator.
+    /// This describes most binary operators, so this helps avoid repetition.
     fn left_associative_binary_op<F, P>(
-        &mut self, next_higher_precedence: F,
-        token_pred: P,
-    ) -> Option<Expr>
+        &mut self,
+        next_higher_precedence: F,
+        op_pred: P,
+    ) -> Result<Expr>
     where
-        F: Fn(&mut Self) -> Option<Expr>,
+        F: Fn(&mut Self) -> Result<Expr>,
         P: Fn(&TokenType) -> bool,
     {
         let mut expr = next_higher_precedence(self)?;
-        while let Some(op) = self.next_token_if(&token_pred) {
+        while let Some(op) = self.next_token_if(&op_pred) {
             let right = next_higher_precedence(self)?;
             expr = Expr::new_binary(expr, op, right);
         }
 
-        Some(expr)
+        Ok(expr)
     }
 
-    fn expression(&mut self) -> Option<Expr> {
+    fn expression(&mut self) -> Result<Expr> {
         self.equality()
     }
 
-    fn equality(&mut self) -> Option<Expr> {
+    fn equality(&mut self) -> Result<Expr> {
         self.left_associative_binary_op(Self::comparison, TokenType::is_equality_op)
     }
 
-    fn comparison(&mut self) -> Option<Expr> {
+    fn comparison(&mut self) -> Result<Expr> {
         self.left_associative_binary_op(Self::term, TokenType::is_comparison_op)
     }
 
-    fn term(&mut self) -> Option<Expr> {
+    fn term(&mut self) -> Result<Expr> {
         self.left_associative_binary_op(Self::factor, TokenType::is_term_op)
     }
 
-    fn factor(&mut self) -> Option<Expr> {
+    fn factor(&mut self) -> Result<Expr> {
         self.left_associative_binary_op(Self::unary, TokenType::is_factor_op)
     }
 
-    fn unary(&mut self) -> Option<Expr> {
+    /// Parses a right-associative unary expression.
+    /// Highest precedence within non-primary expressions.
+    fn unary(&mut self) -> Result<Expr> {
         if let Some(op) = self.next_token_if(TokenType::is_unary_op) {
             let expr = self.unary()?;
-            Some(Expr::new_unary(op, expr))
+            Ok(Expr::new_unary(op, expr))
         } else {
             self.primary()
         }
     }
 
-    fn primary(&mut self) -> Option<Expr> {
+    /// Parses a primary expression.
+    fn primary(&mut self) -> Result<Expr> {
         if let Some(token) = self.next_token() {
             match token.token_type {
-                TokenType::Nil => Some(Expr::new_literal(Value::Nil)),
-                TokenType::True => Some(Expr::new_literal(Value::True)),
-                TokenType::False => Some(Expr::new_literal(Value::False)),
-                TokenType::Number(num) => Some(Expr::new_literal(Value::Number(num))),
-                TokenType::String(str) => Some(Expr::new_literal(Value::String(str))),
+                TokenType::Nil => Ok(Expr::new_literal(Value::Nil)),
+                TokenType::True => Ok(Expr::new_literal(Value::True)),
+                TokenType::False => Ok(Expr::new_literal(Value::False)),
+                TokenType::Number(num) => Ok(Expr::new_literal(Value::Number(num))),
+                TokenType::String(str) => Ok(Expr::new_literal(Value::String(str))),
 
                 TokenType::LeftParen => {
                     let expr = self.expression()?;
-                    if let Some(_) = self.next_token_if(|t| *t == TokenType::RightParen) {
-                        Some(Expr::new_grouping(expr))
-                    } else {
-                        self.log_error(Some(token), "Expected ')' after expression");
-                        None
+                    match self.next_token_if(|t| *t == TokenType::RightParen) {
+                        Some(_) => Ok(Expr::new_grouping(expr)),
+                        None => Err(self.error(Some(token), "Expected ')' after expression"))
                     }
                 }
 
-                _ => {
-                    self.log_error(Some(token), "Expected expression");
-                    None
-                }
+                _ => Err(self.error(Some(token), "Expected expression")),
             }
         } else {
-            self.log_error(None, "Expected expression");
-            None
+            Err(self.error(None, "Expected expression"))
         }
     }
 
+    /// Consumes the next available token and returns it. Returns `None` if there are no tokens.
     fn next_token(&mut self) -> Option<Token> {
         self.tokens.next()
     }
 
+    /// Consumes the next available token and returns it only if `pred` is true for that token.
+    /// Returns `None` if there are no tokens, or if `pred` is false.
     fn next_token_if<F>(&mut self, pred: F) -> Option<Token>
     where
         F: Fn(&TokenType) -> bool,
@@ -112,8 +123,8 @@ impl<'a> Parser<'a> {
         self.tokens.peek().is_none()
     }
 
-    fn log_error(&mut self, token: Option<Token>, message: &str) {
+    fn error(&mut self, token: Option<Token>, message: &str) -> LoxError {
         let line = token.map_or(0, |t| t.line);
-        self.logger.log(LoxError::new(ErrorType::SyntaxError, line, message));
+        LoxError::new(ErrorType::SyntaxError, line, message)
     }
 }
